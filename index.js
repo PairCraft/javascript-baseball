2b3bd1eaac6d38f1ca1b5a16e20fc0e78d964a1a
SSM <ssm102030@naver.com>
2024-11-15 20:09:53 +0900

    함수테스트

---
+++ index.js
@@ -0,0 +1,59 @@
+// PC세 자릿 수 뽑기
+const numberlength = 3;
+console.log("hello")
+function getPCNumber(){
+    let arr_number = [1,2,3,4,5,6,7,8,9];
+    let arr_PCnumber = [3];
+     for(let idx = 0; idx < numberlength; idx++){
+         let randomindex = Math.floor(Math.random()* arr_number.length);/*랜덤으로 뽑는다 0에서 배열 넘버의 사이즈 -1 만큼*/;
+         arr_PCnumber[idx] = arr_number[randomindex];
+         arr_number.splice(randomindex,1);
+    }
+    return arr_PCnumber;
+}
+
+// 유저입력 배열 변환,유저입력 받은 곳 중복 확인 사이즈가 세 자릿 수 인지 확인
+function checkUserNumber(Usernum){
+    if (Usernum.length != numberlength){ 
+        return null;
+    }
+    let arr_temp = new Set(Usernum);
+    if (arr_temp.size != numberlength){
+        return null;
+    }
+    return arr_temp;
+}
+
+// 결과 {결과 실패 -> 힌트, 결과 성공 -> 처음으로 돌아감}
+function compareNumber(pcNum,userNum){
+    let strike = 0;
+    let ball = 0;
+
+    for(let idx = 0; idx < userNum.length; idx++){
+        // 카운트 2 
+        // 같은 값이 있는지 체크 , 인덱스 체크
+        // 같은값 카운트 - 인덱스 카운트
+        console.log(userNum[idx]);
+        let result_idx = pcNum.indexOf(userNum[idx]);        
+        
+        if (result_idx == -1){
+            continue;
+        }
+        if (result_idx == idx){
+            strike +=1;
+        }
+        else if (result_idx != idx){
+            ball += 1;
+        }
+    }
+    
+    if (ball == 0 && strike == 0){
+        console.log('Nothing');
+    }
+    else{
+        console.log('strike =' + strike +'ball =' + ball);
+    }
+}
+console.log(getPCNumber());
+console.log(checkUserNumber('123'));
+compareNumber();

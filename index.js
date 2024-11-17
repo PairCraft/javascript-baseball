c006ef682af48b74f07ad49b33cbbb1fc076d44e
SSM <ssm102030@naver.com>
2024-11-17 23:12:18 +0900

    완성!!!!

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { log } = require('console');
 const readline = require('readline');
 const rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout
 });
-
+const INTERRUPT = 2;
 const NUMBER_LENGTH = 3;
 const GAMESTATE = {
     START:1,
@@ -12,26 +11,38 @@ const GAMESTATE = {
 };
 Object.freeze(GAMESTATE);
 
-function playGame(){
-    rl.question('게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요.',(answer)=>{
-        switch(parseInt(answer)){
-            case GAMESTATE.START:{
-                /*게임 플레이*/             
-                pc_num = getPCNumber();
+//유저입력 받는 함수 -> 입력대기
+async function requestInput(msg){
+    return new Promise((resolve)=>{
+        rl.question(msg,(answer)=>resolve(answer))
+    })
+}
 
-                user_num = requireUserNumber();
-             
-                compareNumber(pc_num, user_num);
+async function playGame(){
+    gamestate = await requestInput('게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요. ')
+        switch(parseInt(gamestate)){
+            case GAMESTATE.START:{             
+                pc_num = getPCNumber();
+                while(gamestate == GAMESTATE.START){
+                    user_num = await requireUserNumber();
+                    if(user_num == INTERRUPT){
+                        console.log('애플리케이션이 종료되었습니다.');
+                        rl.close();
+                        return;
+                    }
+                    compareNumber(pc_num, user_num);
+                }
                 
-                playGame();
+                await playGame();
+                break;
             }
             case GAMESTATE.EXIT:{
                 console.log('애플리케이션이 종료되었습니다.');
+                rl.close();
                 break;
             }
         }
-    });
-}
+    }
 
 function getPCNumber(){
     let arr_number = [1,2,3,4,5,6,7,8,9];
@@ -45,27 +56,25 @@ function getPCNumber(){
     return arr_PCnumber;
 }
 
-const rl2 = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-});
 
-function requireUserNumber(){
-    rl2.question('숫자를 입력해주세요:',(answer)=>{
-        num = checkUserNumber(answer)
-        if(num == null){
-            requireUserNumber();
+
+async function requireUserNumber(){
+    while(true){
+    num = await requestInput('숫자를 입력해주세요: ')
+    if(num == INTERRUPT){
+        return INTERRUPT;
+    }
+    num = checkUserNumber(num);    
+        if(num != null){
+            return num.split('').map(Number);    
         }
-        else{
-            return num;
-        }
-    });
+    }
 }
 
-// 유저입력 배열 변환,유저입력 받은 곳 중복 확인 사이즈가 세 자릿 수 인지 확인
+//세자리 숫자인지 확인
 function checkUserNumber(Usernum){
     if (Usernum.length != NUMBER_LENGTH){ 
-        console.log('${NUMBER_LENGTH}자리 숫자를 입력해주세요.')
+        console.log('세자리 숫자를 입력해주세요.')
         
         return null;
     }
@@ -73,7 +82,6 @@ function checkUserNumber(Usernum){
     return Usernum;
 }
 
-// 결과 {결과 실패 -> 힌트, 결과 성공 -> 처음으로 돌아감}
 function compareNumber(pcNum,userNum){
     let strike_count = 0;
     let ball_count = 0;
@@ -86,16 +94,16 @@ function compareNumber(pcNum,userNum){
 
     if (strike_count == NUMBER_LENGTH){
         console.log(strike_count + '스트라이크');
-        console,log('3개의 숫자를 모두 맞히셨습니다.');
+        console.log('3개의 숫자를 모두 맞히셨습니다.');
         console.log('-------게임 종료-------');
-        playstate = GAMESTATE.EXIT;
+        gamestate = GAMESTATE.EXIT;
+        return;
     }
     if (ball_count == 0 && strike_count == 0){
-        console.log(pcNum);
-        console.log('Nothing');
+
+        console.log('낫싱');
     }
     else{
-        console.log(pcNum);
         console.log( ball_count + '볼 ' + strike_count + '스트라이크');
     }
 }

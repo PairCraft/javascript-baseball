14ab4eda6b306a0ddee2bacaba9caab92f1fc1bf
SSM <ssm102030@naver.com>
2024-12-02 00:15:53 +0900

    완성!

--- gameRecords.js
+++ gameRecords.js
@@ -1,5 +1,5 @@
 // 게임 정보 기록
-function recordGameResult(gameRecords, startTime, inning, inningLimit, isClear){
+function recordGameResult(gameRecords, startTime, inning, inningLimit, isClear) {
     const endTime = new Date();
     gameRecords.push({
         startTime,
@@ -11,9 +11,9 @@ function recordGameResult(gameRecords, startTime, inning, inningLimit, isClear){
 }
 
 // 가장 작은 라운드(이닝) 수와 그에 해당하는 gameId 배열을 반환하는 함수
-function getMinInning(gameRecords){
+function getMinInning(gameRecords) {
     const minInning = gameRecords.reduce((acc, record) => {
-        if (record.inning <= acc){
+        if (record.inning <= acc) {
             return record.inning;
         } else {
             return acc;
@@ -21,20 +21,20 @@ function getMinInning(gameRecords){
     }, Infinity);
 
     let minInningGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inning === minInning){
+        if (record.inning === minInning) {
             acc.push(index + 1);
         }
 
         return acc;
     }, []);
 
-    return {minInning, minInningGameIds};
+    return { minInning, minInningGameIds };
 }
 
 // 가장 큰 라운드(이닝) 수와 그에 해당하는 gameId 배열을 반환하는 함수
-function getMaxInning(gameRecords){
+function getMaxInning(gameRecords) {
     const maxInning = gameRecords.reduce((acc, record) => {
-        if (record.inning >= acc){
+        if (record.inning >= acc) {
             return record.inning;
         } else {
             return acc;
@@ -42,68 +42,65 @@ function getMaxInning(gameRecords){
     }, 0);
 
     let maxInningGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inning === maxInning){
+        if (record.inning === maxInning) {
             acc.push(index + 1);
         }
 
         return acc;
     }, []);
 
-    return {maxInning, maxInningGameIds};
+    return { maxInning, maxInningGameIds };
 }
 
 // 입력제한 횟수들 중 최빈값과 그에 해당하는 gameId 배열을 반환하는 함수
-function getModeInningLimit(gameRecords){
+function getModeInningLimit(gameRecords) {
     // 입력제한 횟수의 빈도수 구하기
     const inningLimitFrequencies = gameRecords.reduce((acc, record, index) => {
-        if (!acc[record.inningLimit]){
-            acc[record.inningLimit] = {frequency: 0, gameIds: []};
+        if (!acc[record.inningLimit]) {
+            acc[record.inningLimit] = { frequency: 0, gameIds: [] };
         }
-
         acc[record.inningLimit].frequency += 1;
         acc[record.inningLimit].gameIds.push(index + 1);
         return acc;
     }, {});
-
+    // 가장큰 최빈값의 값
+    const maxInningLimitFrequency = Math.max(...Object.values(inningLimitFrequencies).map(data => data.frequency));
     // 최빈값과 그에 해당하는 gameId 배열 구하기
-    const modeInningLimit = Object.entries(inningLimitFrequencies).reduce((acc, [inningLimit, frequencyData]) =>{
-        if (frequencyData.frequency >= acc.frequencyData.frequency){
-            return {inningLimit: inningLimit, frequencyData: frequencyData};
+    const modeInningLimit = Object.entries(inningLimitFrequencies).reduce((acc, [inningLimit, frequencyData]) => {
+        if (frequencyData.frequency === maxInningLimitFrequency) {
+            acc.inningLimit.push(inningLimit);
+            acc.frequencyData.gameIds.push(frequencyData.gameIds);
         }
-
         return acc;
-    }, {inningLimit: 0, frequencyData: {frequency: 0, gameIds: []}});
-
+    }, { inningLimit: [], frequencyData: { frequency: 0, gameIds: [] } });
     const resultModeInningLimit = modeInningLimit.inningLimit;
     const resultGameIds = modeInningLimit.frequencyData.gameIds;
-    return {resultModeInningLimit, resultGameIds};
+    return { resultModeInningLimit, resultGameIds };
 }
 
 // 가장 큰 입력제한횟수와 그에 해당하는 gameId 배열을 반환하는 함수
-function getMaxInningLimit(gameRecords){
+function getMaxInningLimit(gameRecords) {
     const maxInningLimit = gameRecords.reduce((acc, record) => {
-        if (record.inningLimit >= acc){
+        if (record.inningLimit >= acc) {
             return record.inningLimit;
         } else {
             return acc;
         }
     }, 0);
-
     let maxInningLimitGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inningLimit === maxInningLimit){
-            return acc.push(index + 1);
+        if (record.inningLimit === maxInningLimit) {
+            acc.push(index + 1);
         }
-        
-        return acc;    
+        return acc;
     }, []);
 
-    return {maxInningLimit, maxInningLimitGameIds};
+    return { maxInningLimit, maxInningLimitGameIds };
 }
 
 // 가장 작은 입력제한횟수와 그에 해당하는 gameId 배열을 반환하는 함수
-function getMinInningLimit(gameRecords){
+function getMinInningLimit(gameRecords) {
     const minInningLimit = gameRecords.reduce((acc, record) => {
-        if (record.inningLimit <= acc){
+        if (record.inningLimit <= acc) {
             return record.inningLimit;
         } else {
             return acc;
@@ -111,15 +108,68 @@ function getMinInningLimit(gameRecords){
     }, Infinity);
 
     let minInningLimitGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inningLimit === minInningLimit){
-            return acc.push(index + 1);
-        } 
-        
+        if (record.inningLimit === minInningLimit) {
+            acc.push(index + 1);
+        }
         return acc;
     }, []);
 
-    return {minInningLimit, minInningLimitGameIds};
+    return { minInningLimit, minInningLimitGameIds };
 }
+
+function getAverageInningLimit(gameRecords) {
+    const sumOfInngLimit = gameRecords.reduce((acc, record) => {
+        return acc + record.inningLimit;
+    }, 0)
+    const averageOfInngLimit = sumOfInngLimit / gameRecords.length;
+    return averageOfInngLimit;
+}
+
+function getModeComputerWinInningLimit(gameRecords) {
+    const computerWinRecords = gameRecords.filter(element => element.isClear === false);
+    const inningLimitFrequencies = computerWinRecords.reduce((acc, record) => {
+        if (!acc[record.inningLimit]) {
+            acc[record.inningLimit] = 0;
+        }
+        acc[record.inningLimit] += 1;
+        return acc;
+    }, {});
+    // 가장큰 최빈값의 값
+    const maxInningLimitFrequency = Math.max(...Object.values(inningLimitFrequencies));
+    const modeInningLimit = Object.entries(inningLimitFrequencies).reduce((acc, [inningLimit, frequency]) => {
+        if (frequency === maxInningLimitFrequency) {
+            acc.push(inningLimit);
+        }
+        return acc;
+    }, []);
+    if(modeInningLimit.length === 0){
+        modeInningLimit.push(0);
+    }
+    return modeInningLimit;
+}
+function getModeUserWinInning(gameRecords) {
+    const userWinRecords = gameRecords.filter(element => element.isClear === true);
+    const inningFrequencies = userWinRecords.reduce((acc, record) => {
+        if (!acc[record.inning]) {
+            acc[record.inning] = 0;
+        }
+        acc[record.inning] += 1;
+        return acc;
+    }, {});
+    // 가장큰 최빈값의 값
+    const maxInningFrequency = Math.max(...Object.values(inningFrequencies));
+    const modeInning = Object.entries(inningFrequencies).reduce((acc, [inning, frequency]) => {
+        if (frequency === maxInningFrequency) {
+            acc.push(inning);
+        } 
+        return acc;
+    }, []);
+    if(modeInning.length === 0){
+        modeInning.push(0);
+    }
+    return modeInning;
+}
+
 
 module.exports = {
     recordGameResult,
@@ -127,5 +177,8 @@ module.exports = {
     getMaxInning,
     getModeInningLimit,
     getMaxInningLimit,
-    getMinInningLimit
+    getMinInningLimit,
+    getAverageInningLimit,
+    getModeComputerWinInningLimit,
+    getModeUserWinInning
 };
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const { recordGameResult, getMinInning, getMaxInning, getModeInningLimit, getMaxInningLimit, getMinInningLimit } = require('./gameRecords');
-const {requestInput, closeInput} = require('./inputHandler');
+const { recordGameResult, getMinInning, getMaxInning, getModeInningLimit, getMaxInningLimit, getMinInningLimit, getAverageInningLimit, getModeComputerWinInningLimit, getModeUserWinInning } = require('./gameRecords');
+const { requestInput, closeInput } = require('./inputHandler');
 
 const NUMBER_LENGTH = 3;
 const GAMESTATE = {
-    START:1,
-    HISTORY:2,
-    STATS:3,
-    EXIT:9
+    START: 1,
+    HISTORY: 2,
+    STATS: 3,
+    EXIT: 9
 };
 Object.freeze(GAMESTATE);
 
-async function playGame(){
+async function playGame() {
     let gameState;
     let gameRecords = [];
-    while (gameState !== GAMESTATE.EXIT){
-        gameState = await requestInput('게임을 새로 시작하려면 1, 종료하려면 9를 입력하세요. ');
+    while (gameState !== GAMESTATE.EXIT) {
+        gameState = await requestInput('게임을 새로 시작하려면 1, 기록을 보려면 2, 통계를 보려면 3, 종료하려면 9을 입력하세요.\n');
         gameState = parseInt(gameState);
-        
+
         await handleGameState(gameState, gameRecords);
     }
 }
 
 // GameState별 분기
 async function handleGameState(gameState, gameRecords) {
-    switch(gameState){
-        case GAMESTATE.START: 
+    switch (gameState) {
+        case GAMESTATE.START:
             await playLoop(gameRecords);
             console.log('-------게임 종료-------');
             break;
-        case GAMESTATE.HISTORY: 
+        case GAMESTATE.HISTORY:
             showHistory(gameRecords);
             break;
         case GAMESTATE.STATS:
@@ -44,66 +44,71 @@ async function handleGameState(gameState, gameRecords) {
     }
 }
 
-function convertTime(time){
+function convertTime(time) {
     const minutes = ('0' + time.getMinutes()).slice(-2);
     return `${time.getFullYear()}. ${time.getMonth()}. ${time.getDate()} ${time.getHours()}:${minutes}`;
 }
 
 // 게임 기록을 보여주는 함수
-function showHistory(gameRecords){ // 기록
+function showHistory(gameRecords) { // 기록
+    let winner = '';
     const history = gameRecords.reduce((acc, record, index) => {
-        return acc + `- [${index+1}] / 시작시간: ${convertTime(record.startTime)} / 종료시간: ${convertTime(record.endTime)} / 횟수: ${record.inningLimit} / 승리자: ${record.isClear}\n`;
+        if (record.isClear) {
+            winner = '사용자';
+        } else {
+            winner = '컴퓨터';
+        }
+        return acc + `- [${index + 1}] / 시작시간: ${convertTime(record.startTime)} / 종료시간: ${convertTime(record.endTime)} / 횟수: ${record.inningLimit} / 승리자: ${winner}\n`;
     }, '');
     console.log(history);
     console.log('-------기록 종료-------');
 }
 
 // 게임 통계를 보여주는 함수
-function showStats(gameRecords){ // 통계
-    const {minInning, minInningGameIds} = getMinInning(gameRecords);
-    const {maxInning, maxInningGameIds} = getMaxInning(gameRecords);
-    const {resultModeInningLimit, resultGameIds} = getModeInningLimit(gameRecords);
-    const {minInningLimit, minInningLimitGameIds} = getMinInningLimit(gameRecords);
-    const {maxInningLimit, maxInningLimitGameIds} = getMaxInningLimit(gameRecords);
+function showStats(gameRecords) { // 통계
+    const { minInning, minInningGameIds } = getMinInning(gameRecords);
+    const { maxInning, maxInningGameIds } = getMaxInning(gameRecords);
+    const { resultModeInningLimit, resultGameIds } = getModeInningLimit(gameRecords);
+    const { minInningLimit, minInningLimitGameIds } = getMinInningLimit(gameRecords);
+    const { maxInningLimit, maxInningLimitGameIds } = getMaxInningLimit(gameRecords);
+    const averageOfInngLimit = getAverageInningLimit(gameRecords);
+    const modeComputerWinInningLimit = getModeComputerWinInningLimit(gameRecords);
+    const modeUserWinInning = getModeUserWinInning(gameRecords);
 
     console.log(`1. 가장 적은 횟수: ${minInning}회 - [${minInningGameIds}]`);
     console.log(`2. 가장 많은 횟수: ${maxInning}회 - [${maxInningGameIds}]`);
     console.log(`3. 가장 많이 적용된 승리/패패 횟수: ${resultModeInningLimit}회 - [${resultGameIds}]`);
     console.log(`4. 가장 큰 값으로 적용된 승리/패패 횟수: ${maxInningLimit}회 - [${maxInningLimitGameIds}]`);
     console.log(`5. 가장 적은 값으로 적용된 승리/패패 횟수: ${minInningLimit}회 - [${minInningLimitGameIds}]`);
-
+    console.log(`6. 적용된 승리/패패 횟수 평균: ${averageOfInngLimit}회`);
+    console.log(`7. 컴퓨터가 가장 많이 승리한 승리/패패 횟수: ${modeComputerWinInningLimit}회`);
+    console.log(`8. 사용자가 가장 많이 승리한 승리/패패 횟수: ${modeUserWinInning}회`);
     console.log('-------통계 종료-------');
-// 6. 적용된 승리/패패 횟수 평균: avg(입력제한)회
-// 7. 컴퓨터가 가장 많이 승리한 승리/패패 횟수: 컴퓨터.승리수 회
-// 8. 사용자가 가장 많이 승리한 승리/패패 횟수: 사용자.승리수 회
-
-
-// 적용된 승리/패패 횟수 평균: 5회
-// 컴퓨터가 가장 많이 승리한 승리/패패 횟수: 0회
-// 사용자가 가장 많이 승리한 승리/패패 횟수: 5회
-
-// 컴퓨터는 -> 최대 입력횟수
-// 사용자는 -> 라운드 수
 }
 
 async function playLoop(gameRecords) {
-    console.log('컴퓨터에게 승리하기 위해 몇번만에 성공해야 하나요?');
-    let inningLimit = await requestInput("");
-    inningLimit = parseInt(inningLimit);
-    // TODO: 0보다 큰 숫자인지 체크
-    
+    let inningLimit;
+    while (true) {
+        inningLimit = await requestInput('컴퓨터에게 승리하기 위해 몇번만에 성공해야 하나요?\n');
+        inningLimit = parseInt(inningLimit);
+        if (inningLimit > 0) {
+            break;
+        } else {
+            console.log('0보다 큰 숫자를 입력해주세요.');
+        }
+    }
     const computerNumber = getComputerNumbers();
     console.log(computerNumber); // 난중에 지울것 답지임
 
     const startTime = new Date();
-    
+
     let inning = 0;
-    while(inning < inningLimit){
+    while (inning < inningLimit) {
         inning++;
-        let userNumber = await requireUserNumbers();  
-        if (checkGameClear(computerNumber, userNumber)){
-            
-            recordGameResult(gameRecords, startTime, inning, inningLimit,true);
+        let userNumber = await requireUserNumbers();
+        if (checkGameClear(computerNumber, userNumber)) {
+
+            recordGameResult(gameRecords, startTime, inning, inningLimit, true);
             console.log("사용자가 승리하였습니다");
             return; // 승리 경우 리턴
         }
@@ -114,10 +119,10 @@ async function playLoop(gameRecords) {
     return; // 패패 경우 리턴
 }
 
-function getComputerNumbers(){
-    let numbers = [1,2,3,4,5,6,7,8,9];
-    let computerNumbers = Array.from({length: NUMBER_LENGTH}, () => {
-        let randomIndex = Math.floor(Math.random()* numbers.length);/*랜덤으로 뽑는다 0에서 배열 넘버의 사이즈 -1 만큼*/ 
+function getComputerNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let computerNumbers = Array.from({ length: NUMBER_LENGTH }, () => {
+        let randomIndex = Math.floor(Math.random() * numbers.length);/*랜덤으로 뽑는다 0에서 배열 넘버의 사이즈 -1 만큼*/
         let selectedNumber = numbers[randomIndex];
         numbers.splice(randomIndex, 1);
         return selectedNumber;
@@ -127,62 +132,62 @@ function getComputerNumbers(){
     return computerNumbers;
 }
 
-async function requireUserNumbers(){
-    while(true){
+async function requireUserNumbers() {
+    while (true) {
         let numbers = await requestInput('숫자를 입력해주세요: ')
         numbers = checkUserNumbers(numbers);
-        if(numbers !== null){
+        if (numbers !== null) {
             return numbers.split('').map(Number);
         }
     }
 }
 
 //세자리 숫자인지 확인
-function checkUserNumbers(userNumbers){
-    if(isNaN(userNumbers)){
+function checkUserNumbers(userNumbers) {
+    if (isNaN(userNumbers)) {
         console.log('숫자를 입력해주세요.');
         return null;
     }
 
-    if(userNumbers.includes('0')){
+    if (userNumbers.includes('0')) {
         console.log('1에서 9까지의 숫자를 입력해주세요');
         return null;
     }
 
     let distinctNumbers = new Set(userNumbers);
-    if (userNumbers.length !== NUMBER_LENGTH || distinctNumbers.size !== NUMBER_LENGTH){
+    if (userNumbers.length !== NUMBER_LENGTH || distinctNumbers.size !== NUMBER_LENGTH) {
         console.log('중복없는 세자리 숫자를 입력해주세요.');
         return null;
     }
 
-    return userNumbers; 
+    return userNumbers;
 }
 
 // 컴퓨터 숫자와 사용자 숫자를 비교하고 strike, ball 카운트롤 반환
-function compareNumber(computerNumbers,userNumbers){
-    const strikes = computerNumbers.filter((element, index)=>userNumbers.indexOf(element)===index);
-    const inclusion = computerNumbers.filter((element)=>userNumbers.includes(element));
+function compareNumber(computerNumbers, userNumbers) {
+    const strikes = computerNumbers.filter((element, index) => userNumbers.indexOf(element) === index);
+    const inclusion = computerNumbers.filter((element) => userNumbers.includes(element));
 
     const strikeCount = strikes.length;
     const ballCount = inclusion.length - strikeCount;
 
-    return {strikeCount, ballCount};
+    return { strikeCount, ballCount };
 }
 
 // 게임을 클리어했는지 체크 (클리어 조건: 3 strikes)
-function checkGameClear(computerNumbers,userNumbers){
-    const {strikeCount, ballCount} = compareNumber(computerNumbers,userNumbers);
+function checkGameClear(computerNumbers, userNumbers) {
+    const { strikeCount, ballCount } = compareNumber(computerNumbers, userNumbers);
 
-    if (strikeCount === NUMBER_LENGTH){
+    if (strikeCount === NUMBER_LENGTH) {
         console.log(strikeCount + '스트라이크');
         console.log('3개의 숫자를 모두 맞히셨습니다.');
         return true;
     }
 
-    if (ballCount === 0 && strikeCount === 0){
+    if (ballCount === 0 && strikeCount === 0) {
         console.log('낫싱');
-    } else { 
-        console.log( ballCount + '볼 ' + strikeCount + '스트라이크');
+    } else {
+        console.log(ballCount + '볼 ' + strikeCount + '스트라이크');
     }
     return false;
 }

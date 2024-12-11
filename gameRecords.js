ff56067cb8cef68bb878db0a60b14520f0bac363
SSM <ssm102030@naver.com>
2024-12-11 16:08:16 +0900

    refactor : reduce 함수 사용 경우 고려 refactor : let const 변수 조절

--- gameRecords.js
+++ gameRecords.js
@@ -20,7 +20,7 @@ function getMinInning(gameRecords) {
         }
     }, Infinity);
 
-    let minInningGameIds = gameRecords.reduce((acc, record, index) => {
+    const minInningGameIds = gameRecords.reduce((acc, record, index) => {
         if (record.inning === minInning) {
             acc.push(index + 1);
         }
@@ -41,7 +41,7 @@ function getMaxInning(gameRecords) {
         }
     }, 0);
 
-    let maxInningGameIds = gameRecords.reduce((acc, record, index) => {
+    const maxInningGameIds = gameRecords.reduce((acc, record, index) => {
         if (record.inning === maxInning) {
             acc.push(index + 1);
         }
@@ -80,95 +80,87 @@ function getModeInningLimit(gameRecords) {
 
 // 가장 큰 입력제한횟수와 그에 해당하는 gameId 배열을 반환하는 함수
 function getMaxInningLimit(gameRecords) {
-    const maxInningLimit = gameRecords.reduce((acc, record) => {
-        if (record.inningLimit >= acc) {
-            return record.inningLimit;
-        } else {
-            return acc;
-        }
-    }, 0);
-    let maxInningLimitGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inningLimit === maxInningLimit) {
-            acc.push(index + 1);
-        }
-        return acc;
-    }, []);
+    const maxInningLimit = Math.max(...gameRecords.map(record => record.inningLimit));
+    const maxInningLimitGameIds = gameRecords
+        .map((record, index) => (record.inningLimit === maxInningLimit ? index + 1 : null))
+        .filter(id => id !== null);
 
     return { maxInningLimit, maxInningLimitGameIds };
 }
 
+
 // 가장 작은 입력제한횟수와 그에 해당하는 gameId 배열을 반환하는 함수
 function getMinInningLimit(gameRecords) {
-    const minInningLimit = gameRecords.reduce((acc, record) => {
-        if (record.inningLimit <= acc) {
-            return record.inningLimit;
-        } else {
-            return acc;
-        }
-    }, Infinity);
-
-    let minInningLimitGameIds = gameRecords.reduce((acc, record, index) => {
-        if (record.inningLimit === minInningLimit) {
-            acc.push(index + 1);
-        }
-        return acc;
-    }, []);
+    const minInningLimit = Math.min(...gameRecords.map(record => record.inningLimit));
+    const minInningLimitGameIds = gameRecords
+        .map((record, index) => (record.inningLimit === minInningLimit ? index + 1 : null))
+        .filter(id => id !== null);
 
     return { minInningLimit, minInningLimitGameIds };
 }
 
+
 function getAverageInningLimit(gameRecords) {
-    const sumOfInngLimit = gameRecords.reduce((acc, record) => {
-        return acc + record.inningLimit;
-    }, 0)
-    const averageOfInngLimit = sumOfInngLimit / gameRecords.length;
-    return averageOfInngLimit;
+    let sumOfInningLimit = 0;
+    let count = 0;
+
+    gameRecords.forEach(record => {
+        sumOfInningLimit += record.inningLimit;
+        count++;
+    });
+
+    const averageOfInningLimit = sumOfInningLimit / count;
+    return averageOfInningLimit;
 }
 
+
 function getModeComputerWinInningLimit(gameRecords) {
+
     const computerWinRecords = gameRecords.filter(element => element.isClear === false);
-    const inningLimitFrequencies = computerWinRecords.reduce((acc, record) => {
-        if (!acc[record.inningLimit]) {
-            acc[record.inningLimit] = 0;
-        }
-        acc[record.inningLimit] += 1;
-        return acc;
-    }, {});
-    // 가장큰 최빈값의 값
-    const maxInningLimitFrequency = Math.max(...Object.values(inningLimitFrequencies));
-    const modeInningLimit = Object.entries(inningLimitFrequencies).reduce((acc, [inningLimit, frequency]) => {
-        if (frequency === maxInningLimitFrequency) {
-            acc.push(inningLimit);
-        }
-        return acc;
-    }, []);
-    if(modeInningLimit.length === 0){
+
+    const inningLimitFrequencies = {};
+    computerWinRecords.forEach(record => {
+        const inningLimit = record.inningLimit;
+        inningLimitFrequencies[inningLimit] = (inningLimitFrequencies[inningLimit] || 0) + 1;
+    });
+
+    const frequencies = Object.values(inningLimitFrequencies);
+    const maxInningLimitFrequency = Math.max(...frequencies);
+
+    const modeInningLimit = Object.keys(inningLimitFrequencies).filter(
+        inningLimit => inningLimitFrequencies[inningLimit] === maxInningLimitFrequency
+    ).map(Number); 
+
+    if (modeInningLimit.length === 0) {
         modeInningLimit.push(0);
     }
+
     return modeInningLimit;
 }
+
 function getModeUserWinInning(gameRecords) {
     const userWinRecords = gameRecords.filter(element => element.isClear === true);
-    const inningFrequencies = userWinRecords.reduce((acc, record) => {
-        if (!acc[record.inning]) {
-            acc[record.inning] = 0;
-        }
-        acc[record.inning] += 1;
-        return acc;
-    }, {});
-    // 가장큰 최빈값의 값
-    const maxInningFrequency = Math.max(...Object.values(inningFrequencies));
-    const modeInning = Object.entries(inningFrequencies).reduce((acc, [inning, frequency]) => {
-        if (frequency === maxInningFrequency) {
-            acc.push(inning);
-        } 
-        return acc;
-    }, []);
-    if(modeInning.length === 0){
+
+    const inningFrequencies = {};
+    userWinRecords.forEach(record => {
+        const inning = record.inning;
+        inningFrequencies[inning] = (inningFrequencies[inning] || 0) + 1;
+    });
+
+    const frequencies = Object.values(inningFrequencies);
+    const maxInningFrequency = Math.max(...frequencies);
+
+    const modeInning = Object.keys(inningFrequencies)
+        .filter(inning => inningFrequencies[inning] === maxInningFrequency)
+        .map(Number);
+
+    if (modeInning.length === 0) {
         modeInning.push(0);
     }
+
     return modeInning;
 }
+
 
 
 module.exports = {
